e62a5df720f79299ef053c8e9b12b4df5cbf7891
veryCrunchy <me@verycrunchy.dev>
2026-08-01 23:12:32 +0200

    fix(files): report transferred placeholder entries

--- ui/src/desktopMain/kotlin/dev/obiente/nextcloudnative/app/WindowsCloudFilesJna.kt
+++ ui/src/desktopMain/kotlin/dev/obiente/nextcloudnative/app/WindowsCloudFilesJna.kt
@@ -180,7 +180,7 @@ internal class JnaWindowsCloudFilesApi : WindowsCloudFilesApi {
             parameters.setLong(PLACEHOLDERS_TOTAL_OFFSET, placeholders.size.toLong())
             parameters.setPointer(PLACEHOLDERS_ARRAY_OFFSET, native.firstPointer)
             parameters.setInt(PLACEHOLDERS_COUNT_OFFSET, placeholders.size)
-            parameters.setInt(PLACEHOLDERS_PROCESSED_OFFSET, 0)
+            parameters.setInt(PLACEHOLDERS_PROCESSED_OFFSET, placeholders.size)
         }
     }
 
